947f0bd61b6c3c32069708b5fc9f63678397a29b
oaymohsin <oaymohsin@gmail.com>
2023-06-05 11:25:26 +0500

    add

--- frontend/src/app/management-module/managementModuleComponents/sign-in/sign-in.component.ts
+++ frontend/src/app/management-module/managementModuleComponents/sign-in/sign-in.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserManagementService } from 'src/app/Shared/Services/user-management.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent {
+public signInForm: FormGroup | any;
 
+constructor(
+  private readonly UserManagementService: UserManagementService,
+  private readonly FormBuilder:FormBuilder,
+  private readonly Router:Router
+){}
+signInFormModel(){
+  this.signInForm=this.FormBuilder.group({
+    email:new FormControl(''),
+    password: new FormControl('')
+  })
+}
+userSignIn(){
+  let userSignInValues= this.signInForm.value;
+  
+}
 }
