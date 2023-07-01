4085309968847be1869227a5b59dcc9e023e25ab
oaymohsin <oaymohsin@gmail.com>
2023-07-01 12:42:14 +0500

    add

--- frontend/src/app/admin/admin-module-components/upload/upload.component.ts
+++ frontend/src/app/admin/admin-module-components/upload/upload.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-upload',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent {
+  @ViewChild('FileSelect') FileSelect:ElementRef | any;
+myuploadform: FormGroup | any;
+imageArray:any =[]
 
+constructor(
+  private formBuilder:FormBuilder
+){this.buildForm()}
+
+buildForm(){
+  this.myuploadform= this.formBuilder.group({
+    title: new FormControl('',[Validators.required]),
+    description: new FormControl('',[Validators.required])
+  })
+}
+
+submitUploadForm(){}
+
+getimages(event:any){
+
+  let files=event.target.files;
+  const filelength=files.length;
+
+  if(filelength<=5){
+    this.imageArray.push(...files);
+  }
+  else{
+    this.imageArray=[]
+    this.FileSelect.nativeElement.value=null;
+  }
+}
 }
--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { DashboardComponent } from './admin-module-components/dashboard/dashboard.component';
 import { UploadComponent } from './admin-module-components/upload/upload.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +14,9 @@ import { UploadComponent } from './admin-module-components/upload/upload.compone
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class AdminModule { }
